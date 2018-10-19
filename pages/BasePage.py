3328d23e804a872c6cf41fc3d1487eda3cfaae37
chuanlin.li <chuanlin.li@huilianyi.com>
2018-10-19 16:31:55 +0800

    20181019

---
+++ pages/BasePage.py
@@ -0,0 +1,63 @@
+# coding = utf-8
+
+'''
+created on Sep.21 2018
+@author: chuanlin
+description: 基类页面，封装其他所有页面所用到的公用属性和方法
+'''
+
+
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+
+class BasePage(object):
+
+    def __init__(self, seleniumDriver, baseUrl):
+        self.driver = seleniumDriver
+        self.baseUrl = baseUrl
+        #self.pageTitle = pageTitle
+
+    # 通过title断言进入的页面是否正确
+    def check_page_title(self, pageTitle):
+        return pageTitle in self.driver.title
+
+    # 打开页面，并校验页面链接是否加载正确
+    def _open(self, url):
+        self.driver.get(url)
+        self.driver.maximize_window()
+
+        # assert self.checkPageTitle(pageTitle), u"打开页面失败 %s"%url
+
+    # 定义open方法，调用_open()进行打开链接
+    def open(self):
+        self._open(self.baseUrl)
+
+    # 定位页面元素的方法
+    def findElement(self, loc):
+        try:
+            WebDriverWait(self.driver, 100).until(ec.visibility_of_element_located(loc))
+            return self.driver.find_element(*loc)
+        except:
+            print("未找到元素")
+
+    # 封装send_keys方法
+    def sendKeys(self, loc, value, locateFirst=False, clearFirst=False):
+        try:
+            loc = getattr(self, "_%s"%loc)
+            if locateFirst:
+                self.findElement(*loc).click()
+            if clearFirst:
+                self.findElement(*loc).clear()
+
+            self.findElement(*loc).send_keys(value)
+        except AttributeError:
+            print ("页面中未找到元素")
+
+    # 重写switch_frame方法
+    def switchFrame(self, loc):
+        return self.driver.switch_to_frame(loc)
+
+    # 定义script方法，用于执行js脚本，返回执行结果
+    def script(self, src):
+        self.driver.execute_script(src)
---
+++ pages/LoginPage.py
@@ -0,0 +1,59 @@
+# code=utf-8
+
+'''
+created on Sep.28 2018
+@author: chuanlin
+description: 中控登录页面的元素与操作方法
+'''
+
+from selenium.webdriver.common.by import By
+from BasePage import BasePage
+
+
+class LoginPage(BasePage):
+
+    # #############页面上的元素######################
+    # 登录框
+    username_loc = (By.CSS_SELECTOR, "input[type='text']")
+    # 密码框
+    password_loc = (By.CSS_SELECTOR, "input[type='password']")
+    # 登录按钮
+    loginButton_loc = (By.CSS_SELECTOR, "button[type='button']")
+
+    # 企业登录跳转按钮
+    companyLogin_loc = (By.XPATH, "//*[@id='app']/div/div[1]/div[2]/div[1]/div/div/div[1]")
+    # 普通登录跳转按钮（点击企业登录后出现）
+    normalLogin_loc = (By.XPATH, "//*[@id='app']/div/div[1]/div[2]/div[1]/div/div/div/div[1]")
+    # 找回密码按钮
+    forgetPassword_loc = (By.XPATH, "//*[@id='app']/div/div[1]/div[2]/div[1]/div/div/div[2]")
+    # 返回登录按钮（点击忘记密码之后出现）
+    backToLogin_loc = (By.XPATH, "//*[@id='app']/div/div[1]/div[3]/div[2]/a")
+    # 企业登陆账号输入框
+    companyLoginUsername_loc = (By.XPATH, "//*[@id='app']/div/div[1]/div[2]/div[1]/div/div/input")
+    # 企业登陆的下一步按钮
+    nextStep_loc = (By.XPATH, "//*[@id='app']/div/div[1]/div[2]/div[1]/div/div/button")
+
+    # #####################元素操作###########################
+    # 输入用户名
+    def input_username(self, username):
+        self.findElement(self.username_loc).clear()
+        self.findElement(self.username_loc).send_keys(username)
+
+    # 输入密码
+    def input_password(self, password):
+        self.findElement(self.password_loc).send_keys(password)
+
+    # 点击登录按钮
+    def click_loginButton(self):
+        self.findElement(self.loginButton_loc).click()
+
+    # 普通登录组合方法
+    def login(self, username, password):
+        self.open()
+        self.input_username(username)
+        self.input_password(password)
+        self.click_loginButton()
+
+    # 打开网页
+    def open(self):
+        self._open(self.baseUrl)
